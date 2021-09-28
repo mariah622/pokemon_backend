2ca988775564cf641bfff091bfb87b328cdc7e8d
mariah622 <mjustine622@gmail.com>
2021-09-27 20:05:02 -0400

    added if statement for create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,21 @@ class CommentsController < ApplicationController
         render json: comments
     end 
 
-    def create
+    def create 
         comment = Comment.new(comment_params)
-    end 
-    
+        # byebug
+        if comment.save
+            render json: comment
+        else
+            render json: {error: "Could not create comment."} 
+        end 
+
+    end
     
     
     private
 
-    def comment_params
-    end 
+    def comment_params 
+        params.require(:comment).permit(:title, :description, :pokemon_id)
+    end
 end
