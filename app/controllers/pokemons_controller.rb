f1f816f038fb85842ea2e53a3f02a750dec69345
mariah622 <mjustine622@gmail.com>
2021-10-03 17:15:25 -0400

    crud actions for pokemon added and working successfully

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,4 +3,37 @@ class PokemonsController < ApplicationController
         pokemons = Pokemon.all
         render json: pokemons
     end 
+
+    def create 
+        pokemon = Pokemon.new(pokemon_params)
+        # byebug
+        if pokemon.save
+            render json: pokemon
+        else
+            render json: {error: "Could not create pokemon."} 
+        end 
+
+    end
+
+    def update 
+        pokemon = Pokemon.find_by_id(params[:id])
+        if pokemon.update(pokemon_params)
+            render json: pokemon
+        else 
+            render json: {error: "Could not pokemon comment. Try again."}
+        end 
+    end 
+
+    def destroy
+        pokemon = Pokemon.find_by_id(params[:id])
+        pokemon.destroy
+        render json: {message: "Pokemon has been deleted succesfully!"}
+    end 
+
+
+    private
+
+    def pokemon_params 
+        params.require(:pokemon).permit(:id, :name, :move, :ability)
+    end
 end
